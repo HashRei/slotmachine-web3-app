f94031497213e6bffb97fc6f5acf883019b02703
HashRei <silvan.reigue@protonmail.com>
2022-03-28 17:43:19 +0200

    Update naming of the token and add initial supply

---
+++ contracts/SMT.sol
@@ -0,0 +1,11 @@
+// contracts/SMTToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SMT is ERC20 {
+    constructor() ERC20("SlotMachineToken", "SMT") {
+        _mint(msg.sender, 10000);
+    }
+}

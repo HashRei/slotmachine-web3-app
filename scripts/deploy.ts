eed90cc97d73b5c7889975a1c46e268c0356c467
HashRei <silvan.reigue@protonmail.com>
2022-03-28 20:47:10 +0200

    Update prettierrc

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,17 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 async function main() {
-  const SMT = await ethers.getContractFactory("SMT");
-  const smt = await SMT.deploy();
+  const SMT = await ethers.getContractFactory("SMT")
+  const smt = await SMT.deploy()
 
-  await smt.deployed();
+  await smt.deployed()
 
-  console.log("SlotMachineToken deployed to:", smt.address);
+  console.log("SlotMachineToken deployed to:", smt.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
---
+++ test/SMT.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai"
+import hre, { ethers } from "hardhat"
+import { Contract } from "ethers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+before(async function () {
+  let owner: SignerWithAddress
+})
+describe("SlotMachineToken", function () {
+  it("Should return the new greeting once it's changed", async function () {})
+})

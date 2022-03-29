88740ed1f50c9b59e9e920d8cf0a562a0ffa7b70
HashRei <silvan.reigue@protonmail.com>
2022-03-29 08:45:34 +0200

    Add SMT token tests and update tsconfig.json

--- test/SMT.test.ts
+++ test/SMT.test.ts
@@ -1,11 +1,52 @@
-import { expect } from "chai"
-import hre, { ethers } from "hardhat"
-import { Contract } from "ethers"
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import {expect, use} from 'chai';
+import {Contract} from 'ethers';
+import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import SMT from "../artifacts/contracts/SMT.sol/SMT.json";
 
-before(async function () {
-  let owner: SignerWithAddress
-})
-describe("SlotMachineToken", function () {
-  it("Should return the new greeting once it's changed", async function () {})
-})
+
+use(solidity);
+
+describe('SlotMachineToken', () => {
+  const [wallet, walletTo] = new MockProvider().getWallets();
+  let token: Contract;
+  const initialSupply = 10000;
+
+  beforeEach(async () => {
+    token = await deployContract(wallet, SMT, [initialSupply]);
+  });
+
+  it('Assigns initial balance', async () => {
+    expect(await token.balanceOf(wallet.address)).to.equal(initialSupply);
+  });
+
+  it('Transfer adds amount to destination account', async () => {
+    await token.transfer(walletTo.address, 7);
+    expect(await token.balanceOf(walletTo.address)).to.equal(7);
+  });
+
+  it('Transfer emits event', async () => {
+    await expect(token.transfer(walletTo.address, 7))
+      .to.emit(token, 'Transfer')
+      .withArgs(wallet.address, walletTo.address, 7);
+  });
+
+  it('Can not transfer above the amount', async () => {
+    await expect(token.transfer(walletTo.address, initialSupply + 7)).to.be.reverted;
+  });
+
+  it('Can not transfer from empty account', async () => {
+    const tokenFromOtherWallet = token.connect(walletTo);
+    await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
+      .to.be.reverted;
+  });
+
+  it('Calls totalSupply on SMT contract', async () => {
+    await token.totalSupply();
+    expect('totalSupply').to.be.calledOnContract(token);
+  });
+
+  it('Calls balanceOf with sender address on SMT contract', async () => {
+    await token.balanceOf(wallet.address);
+    expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
+  });
+});

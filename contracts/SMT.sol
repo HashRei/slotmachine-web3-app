50d1fc3f040e456a8d412b29a066feed51349311
HashRei <silvan.reigue@protonmail.com>
2022-03-29 08:44:50 +0200

    Add initialSupply argument to constructor

--- contracts/SMT.sol
+++ contracts/SMT.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SMT is ERC20 {
-  constructor() ERC20("SlotMachineToken", "SMT") {
-    _mint(msg.sender, 10000);
+  constructor(uint256 initialSupply) ERC20("SlotMachineToken", "SMT") {
+    _mint(msg.sender, initialSupply);
   }
 }

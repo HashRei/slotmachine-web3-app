5f1c346452df0ab21865aab7de76d4be88507c54
HashRei <silvan.reigue@protonmail.com>
2022-04-07 09:17:55 +0200

    Update subscription methods for Chainlink VRF

--- contracts/SlotMachine.sol
+++ contracts/SlotMachine.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
@@ -10,7 +11,7 @@ contract SlotMachine is VRFConsumerBaseV2, ReentrancyGuard, Ownable {
   VRFCoordinatorV2Interface COORDINATOR;
   LinkTokenInterface LINKTOKEN;
 
-  /** CONSTANTS **/
+  /** VRF CONSTANTS **/
 
   // BSC Testnet LINK token contract address
   address public LINK_TOKEN_ADDRESS = 0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06;
@@ -19,20 +20,26 @@ contract SlotMachine is VRFConsumerBaseV2, ReentrancyGuard, Ownable {
   address public VRF_COORDINATOR = 0x6A2AAd07396B36Fe02a22b33cf443582f682c82f;
 
   // The gas lane to use, which specifies the maximum gas price to bump to.
-  // For a list of available gas lanes on each network,
-  // see https://docs.chain.link/docs/vrf-contracts/#configurations
   bytes32 KEY_HASH = 0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314;
 
-  // A reasonable default is 100000, but this value could be different
-  // on other networks. Is good in our case https://bscscan.com/chart/gasprice
-  uint32 CALLBACK_GAS_LIMIT = 100000;
+  // Maximum amount of gas  that can be spend on the callback request
+  uint32 CALLBACK_GAS_LIMIT = 100000; // Unit is gas
 
-  // The default is 3, but you can set this higher.
   uint16 REQUEST_CONFIRMATIONS = 3;
 
   // For this example, retrieve 1 random value in one request.
   // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
   uint32 NUM_WORDS = 1;
+
+  uint16 GAS_PRICE = 10.5; // 10.5 Gwei
+
+  uint16 VERIFICATION_GAS = 200000; // Unit is gas
+
+  // https://docs.chain.link/docs/chainlink-vrf/#subscription-billing
+  uint256 TOTAL_GAS_COST = GAS_PRICE * (VERIFICATION_GAS + CALLBACK_GAS_LIMIT);
+
+  // https://docs.chain.link/docs/vrf-contracts/#configurations
+  uint16 LINK_PREMIUM = 0.005; // Unit is LINK
 
   /** VARIABLES **/
 
@@ -48,37 +55,66 @@ contract SlotMachine is VRFConsumerBaseV2, ReentrancyGuard, Ownable {
   uint64 public s_subscriptionId;
   address s_owner; // Address of the smart contract owner
 
-  // events
+  /**  Events **/
   event AddedLiquidity(uint256 smtAmount, uint256 bnbAmount);
 
+  /** CONSTRUCTOR **/
+
   constructor() {
-    COORDINATOR = VRFCoordinatorV2Interface(VRF_COORDINATOR);
+    COORDINATOR = VRFCoordinatorV2Interface(VRF_COORDINATOR); // Processes the random number request and determines the final charge of it
     LINKTOKEN = LinkTokenInterface(LINK_TOKEN_ADDRESS);
     s_owner = msg.sender;
     player = msg.sender;
-    //Create a new subscription when you deploy the contract.
+    // Create a new subscription the contract is deployed
     createNewSubscription();
+  }
+
+  /** MAIN METHODS **/
+
+  receive() external payable {}
+
+  // Withdraw all the SMT tokens from the current player
+  function withdrawAllTokens() public nonReentrant {
+    require(msg.sender == player, "You are not the player");
+    require(tokenBalanceToPlay[msg.sender] > 0, "No SMT tokens in the machine");
+
+    //Send SMT tokens from the player which are in the machine to the players wallet
+
+    IERC20(smt).transfer(msg.sender, tokenBalanceToPlay[msg.sender]);
+  }
+
+  // Get a random value from Chainlink VRF and check if the player has won
+  function spin() public payable {
+    require(tokenBalanceToPlay[msg.sender] >= 1, "Token balance too low");
+
+    tokenBalanceToPlay[msg.sender] -= 1;
   }
 
   /** VRF METHODS **/
 
-  // Assumes the subscription is funded sufficiently.
-  function requestRandomWords() external onlyOwner {
+  // Assumes this contract owns link
+  // Sends fund the subscription contract and request a random value
+  function fundAndRequestRandomWords() external onlyOwner {
+    // LINK cost hestimation times two to be on the safe side
+    uint256 memory _amount = (TOTAL_GAS_COST + LINK_PREMIUM) * 2;
+    // Sends LINK tokens from this contract to the COORDINATOR/subscription that allows the requesting of the random number
+    LINKTOKEN.transferAndCall(address(COORDINATOR), _amount, abi.encode(s_subscriptionId));
     // Will revert if subscription is not set and funded.
     s_requestId = COORDINATOR.requestRandomWords(
       KEY_HASH,
       s_subscriptionId,
-      REQUESTCONFIRMATIONS,
+      REQUEST_CONFIRMATIONS,
       CALLBACK_GAS_LIMIT,
       NUM_WORDS
     );
   }
 
+  // Callback function to receive the random value
   function fulfillRandomWords(
     uint256, /* requestId */
     uint256[] memory randomWords
   ) internal override {
-    randomResult = (randomWords[0] % 2);
+    randomResult = (randomWords[0] % 2); // Number is either 0 or 1
   }
 
   // Create a new subscription when the contract is initially deployed.
@@ -89,12 +125,6 @@ contract SlotMachine is VRFConsumerBaseV2, ReentrancyGuard, Ownable {
     s_subscriptionId = COORDINATOR.createSubscription();
     // Add this contract as a consumer of its own subscription.
     COORDINATOR.addConsumer(s_subscriptionId, consumers[0]);
-  }
-
-  // Assumes this contract owns link.
-  // 1000000000000000000 = 1 LINK
-  function topUpSubscription(uint256 amount) external onlyOwner {
-    LINKTOKEN.transferAndCall(address(COORDINATOR), amount, abi.encode(s_subscriptionId));
   }
 
   function addConsumer(address consumerAddress) external onlyOwner {
@@ -117,26 +147,5 @@ contract SlotMachine is VRFConsumerBaseV2, ReentrancyGuard, Ownable {
   // 1000000000000000000 = 1 LINK
   function withdraw(uint256 amount, address to) external onlyOwner {
     LINKTOKEN.transfer(to, amount);
-  }
-
-  /** MAIN METHODS **/
-
-  receive() external payable {}
-
-  // Withdraw all the SMT tokens from the current player
-  function withdrawAllTokens() public nonReentrant {
-    require(msg.sender == player, "You are not the player");
-    require(tokenBalanceToPlay[msg.sender] > 0, "No SMT tokens in the machine");
-
-    //Send SMT tokens from the player which are in the machine to the players wallet
-
-    IERC20(smt).transfer(msg.sender, tokenBalanceToPlay[msg.sender]);
-  }
-
-  // Get a random value from Chainlink VRF and check if the player has won
-  function spin() public payable {
-    require(tokenBalanceToPlay[msg.sender] >= 1, "Token balance too low");
-
-    tokenBalanceToPlay[msg.sender] -= 1;
   }
 }
